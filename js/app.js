eac8d82f5e141d1331ea3c4aee06dfafadb70b92
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-27 09:33:32 +0300

    initCommit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+"use strict"; // here we go again
+
+console.log("app.js");
+import Todo from "./class/todo.js";
+
+let t1 = new Todo("read a book");
+let t2 = new Todo("read a magazine");
+
+console.log(t1, t2);
---
+++ js/class/todo.js
@@ -0,0 +1,10 @@
+export default class Todo {
+  static todoCount = 0;
+  constructor(title) {
+    //id, title, done, timeStamp
+    this.id = "t_" + ++Todo.todoCount;
+    this.title = title;
+    this.done = false;
+    this.timeStamp = new Date();
+  }
+}
